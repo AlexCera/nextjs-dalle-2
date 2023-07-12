40488bd2566148da4f2859af34423d5ebd9353d7
= <=>
2023-07-12 12:10:31 -0500

    (feat) Simple endpoint to generate the image with ai.

--- src/app/api/generate/route.ts
+++ src/app/api/generate/route.ts
@@ -1,7 +1,29 @@
 import { NextResponse } from "next/server";
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY
+})
+
+const openai = new OpenAIApi(configuration)
+
 
 export async function POST(req: Request) {
-    const body = await req.json();
+    const { prompt } = await req.json();
 
-    return NextResponse.json({ message: "API works" })
+    if (!prompt || prompt.length === 0) {
+        return NextResponse.json(
+            { message: "You must provied a prompt", statusCode: 400 },
+            { status: 400 }
+        )
+    }
+
+    /* Solicitud para generar la imagen */
+    const aiResponse = await openai.createImage({
+        prompt,
+        n: 1,
+        size: "512x512"
+    })
+
+    return NextResponse.json({ url: aiResponse.data.data[0].url }, { status: 200 })
 }
